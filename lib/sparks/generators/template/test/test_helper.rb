5430afd22e31bab2151c22620125c15c4d9b58db
Robert Evans <robert@codewranglers.org>
2013-08-22 17:51:46 -0700

    updated the config/application so the application configuration is now a module that can be reused within the Modular Sinatra Applications. That means we can have more than one Sinatra application.
    Updated the test_helper to include Bundler loading for the test env.

--- lib/sparks/generators/template/test/test_helper.rb
+++ lib/sparks/generators/template/test/test_helper.rb
@@ -1,8 +1,23 @@
+# set the environment
 ENV['RACK_ENV'] = 'test'
 
+# Load Bundler and Gemfile with the "test" env
 require 'bundler'
 Bundler.require(:default, ENV["RACK_ENV"])
 
+# explicitly require testing frameworks
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'rack/test'
+
+# Application Configuration
+require "./config/application"
+
+# Applications
+require "./app/api"
+
+# Include the Rack Test Methods within
+# Minitest.
+class Minitest::Test
+  include Rack::Test::Methods
+end
