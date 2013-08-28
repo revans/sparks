cd360157f9ae24a6c0e51f88cfaaf78dfd8d5135
Robert Evans <robert@codewranglers.org>
2013-08-28 12:20:06 -0700

    updated the test helper to include some methods to make setting up a rack builder in tests a bit easier.

--- lib/sparks/generators/template/test/test_helper.rb
+++ lib/sparks/generators/template/test/test_helper.rb
@@ -16,8 +16,57 @@ require "./config/application"
 # Applications
 require "./apps/api"
 
+module BuilderDsl
+  def route_to(url, app)
+    map(url) { run app.new }
+  end
+end
+
+class Rack::Builder
+  include BuilderDsl
+end
+
+module BuilderHelper
+  include BuilderDsl
+
+  def builder(&block)
+    build = ::Rack::Builder.new
+    build.instance_eval(&block)
+    build.to_app
+  end
+end
+
 # Include the Rack Test Methods within
 # Minitest.
 class Minitest::Test
   include Rack::Test::Methods
 end
+
+# When testing Sinatra apps, you'll need to
+# setup an +app+ method. There's a couple
+# of convenient methods for doing so. Here's
+# and example:
+
+# module MySinatraApplication
+#   class ApiTest < Minitest::Test
+#     def app
+#       builder do
+#         route_to "/api/v1", Api
+#       end
+#     end
+#   end
+# end
+
+# The +app+ method above is the equivelant of doing:
+
+# module MySinatraApplication
+#   class ApiTest < Minitest::Test
+#     def app
+#       ::Rack::Builder.new do
+#         map "/api/v1" do
+#           run Api.new
+#         end
+#       end.to_app
+#     end
+#   end
+# end
